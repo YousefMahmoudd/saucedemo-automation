cbe30013f930819a5b982c99f6f10fce6174c31c
Youssef Mahmoud <127309810+YousefMahmoudd@users.noreply.github.com>
2026-09-21 17:03:28 +0300

    Update InventoryTest.java

--- InventoryTest.java
+++ InventoryTest.java
@@ -11,7 +11,7 @@ import java.util.Comparator;
 import java.util.List;
 
 /**
- * Step 2: Product Inventory and Sorting.
+  Product Inventory and Sorting
  */
 public class InventoryTest extends BaseTest {
 
