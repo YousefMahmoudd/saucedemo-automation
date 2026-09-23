a24d26516c94be520eb25f903f6ceba9354fdaaa
Youssef Mahmoud <127309810+YousefMahmoudd@users.noreply.github.com>
2026-09-23 14:41:28 +0300

    Update CheckoutStepTwoPage.java

--- CheckoutStepTwoPage.java
+++ CheckoutStepTwoPage.java
@@ -1,9 +1,7 @@
 package com.saucedemo.pages;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
